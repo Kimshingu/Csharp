ba8b8a94e3ead3240901095c2bd51e8ca925c403
KIM Shingu <creation07@naver.com>
2018-10-15 15:28:07 +0900

    second c# application

---
+++ HelloWorld/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            short a = 10;
+            int b = a;
+            int c = 50000;
+            try
+            {
+                short d = checked((short)c);
+                Console.WriteLine("short: {0}", d);
+            }
+            catch (Exception e) {
+                Console.WriteLine("예외: {0}", e.StackTrace);
+                Console.WriteLine("예외: "+ e.StackTrace);
+            }
+
+            var i = 999; 
+            // Var는 동일한 문에서 지역 변수를 선언하고 초기화하는 경우에만 사용할 수 있다. 
+        }
+    }
+}
---
+++ HelloWorld/ConsoleApp2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class MYConstanrt
+    {
+        public const double PI = 3.14; // java의 static final
+        public const int MYAGE = 28;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            double radius = 2;
+            double area = MYConstanrt.PI * (radius * radius);
+            Console.WriteLine("Area = {0}, Age={1}", area, MYConstanrt.MYAGE);
+
+            const String name = "홍길동";
+            Console.WriteLine("name = " + name);
+
+        }
+    }
+}
