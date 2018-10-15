4a60908c998de43a042c05400e0d20ca1548c9a1
KIM Shingu <creation07@naver.com>
2018-10-15 14:13:00 +0900

    first c# application

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello C#~" + args[0]);
+
+        }
+    }
+}
